798eeb4929add0d850ec6e09a843e91eef7e17ab
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-24 16:31:33 -0400

    Removed redundant qualifier name

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/Greeting.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/Greeting.kt
@@ -1,7 +1,7 @@
 package com.catchersmittsoftware.games.kanjiemoji
 
 class Greeting {
-    private val platform = _root_ide_package_.com.catchersmittsoftware.games.kanjiemoji.getPlatform()
+    private val platform = getPlatform()
 
     fun greet(): String {
         return "Hello, ${platform.name}!"
