70830c67051db62f8ee8e9bff1fbb525970184b6
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-24 16:30:46 -0400

    Corrected path to getPlatform( )

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/Greeting.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/Greeting.kt
@@ -1,7 +1,7 @@
 package com.catchersmittsoftware.games.kanjiemoji
 
 class Greeting {
-    private val platform = _root_ide_package_.com.catchersmittsoftware.kanjiemojigame.getPlatform()
+    private val platform = _root_ide_package_.com.catchersmittsoftware.games.kanjiemoji.getPlatform()
 
     fun greet(): String {
         return "Hello, ${platform.name}!"
