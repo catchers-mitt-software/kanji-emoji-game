d5877c4accc07eb74d9c9ee8b653193b5b617a18
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-25 00:43:28 -0400

    Failing low BMP characters is not CJK ideograph test

--- composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifierTest.kt
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifierTest.kt
@@ -4,9 +4,16 @@ import com.catchersmittsoftware.text.isCJKIdeographic
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class CharacterTypeIdentifierTest {
+
+    private fun chooseLowBMPChar(): Char {
+        var propChar = (' ' .. '\u2DFF').random()
+        while (!propChar.isDefined()) propChar++
+        return propChar
+    }
 
     @Test
     fun testIsCJKIdeographic() {
@@ -14,6 +21,13 @@ class CharacterTypeIdentifierTest {
         val ch: Char = ('\u4E00' .. '\u9FFF').random()
         val message = "Character $ch should be CJK ideograph"
         assertTrue(isCJKIdeographic(ch), message)
+    }
+
+    @Test
+    fun testLowBMPCharIsNotCJKIdeographic() {
+        val ch = chooseLowBMPChar()
+        val message = "Character $ch should not be CJK ideograph"
+        assertFalse(isCJKIdeographic(ch), message)
     }
 
 }
