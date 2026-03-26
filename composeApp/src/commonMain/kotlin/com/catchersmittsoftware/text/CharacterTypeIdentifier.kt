dcb6ae37f7d99db3799fd70a4e3cff588eedf579
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-25 23:41:42 -0400

    Passing CJK Symbols and Punctuation are not kanji test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -1,5 +1,5 @@
 package com.catchersmittsoftware.text
 
-fun isCJKIdeographic(ch: Char): Boolean = ch > '\u2FFF'
+fun isCJKIdeographic(ch: Char): Boolean = ch > '\u303F'
 
 fun isEmoji(ch: Char): Boolean = false
