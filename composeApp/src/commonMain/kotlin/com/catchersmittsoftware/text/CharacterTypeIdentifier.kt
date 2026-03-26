df837dbc301902bc04e26d3ab378e0104e697505
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-26 00:01:15 -0400

    Passing bopomofo extended are not hanzi test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -1,5 +1,5 @@
 package com.catchersmittsoftware.text
 
-fun isCJKIdeographic(ch: Char): Boolean = ch > '\u318E'
+fun isCJKIdeographic(ch: Char): Boolean = ch > '\u31BF'
 
 fun isEmoji(ch: Char): Boolean = false
