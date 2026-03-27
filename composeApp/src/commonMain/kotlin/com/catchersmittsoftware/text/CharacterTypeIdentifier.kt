c0d326458948e39833331e7b414b5813f1e929fe
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-26 23:57:32 -0400

    Passing circled ARIB STD B24 kanji are kanji test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -1,7 +1,7 @@
 package com.catchersmittsoftware.text
 
 fun isCJKIdeographic(ch: Char): Boolean {
-    if (ch in '\u3220'..'\u3243') {
+    if (ch in '\u3220'..'\u3247') {
         return true
     } else {
         return ch > '\u3247'
