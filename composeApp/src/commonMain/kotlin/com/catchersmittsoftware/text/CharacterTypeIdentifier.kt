9e1c83669bfc26211482fff0f6e50faa5e1490fb
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-26 21:25:44 -0400

    Passing katakana phonetic extensions are not kanji test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -1,5 +1,5 @@
 package com.catchersmittsoftware.text
 
-fun isCJKIdeographic(ch: Char): Boolean = ch > '\u31BF'
+fun isCJKIdeographic(ch: Char): Boolean = ch > '\u31FF'
 
 fun isEmoji(ch: Char): Boolean = false
