36a0676bd8a80ce9e4bdc2c218bfb8888e5b402e
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-26 21:44:10 -0400

    Passing parenthesized kanji are kanji test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -1,5 +1,5 @@
 package com.catchersmittsoftware.text
 
-fun isCJKIdeographic(ch: Char): Boolean = ch > '\u3247'
+fun isCJKIdeographic(ch: Char): Boolean = ch > '\u3220'
 
 fun isEmoji(ch: Char): Boolean = false
