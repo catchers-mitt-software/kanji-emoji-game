1b473b762acf9884e1feb24d1408175e328c1cc6
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-27 00:13:53 -0400

    Passing circled numbers are not kanji test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -4,7 +4,7 @@ fun isCJKIdeographic(ch: Char): Boolean {
     if (ch in '\u3220'..'\u3247') {
         return true
     } else {
-        return ch > '\u3250'
+        return ch > '\u325F'
     }
 }
 
