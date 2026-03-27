12865f2cf89930a8293360c5bac54ffa6a457db7
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-26 21:55:34 -0400

    Preparing to fail the next few tests

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -1,5 +1,11 @@
 package com.catchersmittsoftware.text
 
-fun isCJKIdeographic(ch: Char): Boolean = ch > '\u3220'
+fun isCJKIdeographic(ch: Char): Boolean {
+    if (ch > '\u3220' && ch <= '\u3243') {
+        return true
+    } else {
+        return ch > '\u3247'
+    }
+}
 
 fun isEmoji(ch: Char): Boolean = false
