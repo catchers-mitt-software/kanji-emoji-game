4547b1919924c195803888148c97481aba1c9215
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-26 21:32:01 -0400

    Passing parenthesized hangul are not kanji test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -1,5 +1,5 @@
 package com.catchersmittsoftware.text
 
-fun isCJKIdeographic(ch: Char): Boolean = ch > '\u31FF'
+fun isCJKIdeographic(ch: Char): Boolean = ch > '\u321E'
 
 fun isEmoji(ch: Char): Boolean = false
