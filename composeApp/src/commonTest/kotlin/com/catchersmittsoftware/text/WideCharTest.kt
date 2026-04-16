317db8fb49567d681e9d77ceba57ed46335450ce
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-16 00:35:07 -0400

    New class

---
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
@@ -0,0 +1,9 @@
+package com.catchersmittsoftware.text
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class WideCharTest {
+}
