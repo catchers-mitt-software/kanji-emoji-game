bfe23e60be352a3693fb3d215b08b4a9f9583a5c
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-21 14:14:29 -0400

    Couple of stubs

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/ui/theme/AppColorSchemes.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/ui/theme/AppColorSchemes.kt
@@ -1,4 +1,30 @@
 package com.catchersmittsoftware.games.kanjiemoji.ui.theme
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.ColorScheme
+
+// TODO: Figure out if this is the right way to go about this
 object AppColorSchemes {
+
+    // TODO: Choose distinct colors
+    val lightColorScheme: ColorScheme = ColorScheme(Color(0), Color(0),
+        Color(0), Color(0), Color(0), Color(0), Color(0), Color(0), Color(0),
+        Color(0), Color(0), Color(0), Color(0), Color(0), Color(0), Color(0),
+        Color(0), Color(0), Color(0), Color(0), Color(0), Color(0), Color(0),
+        Color(0), Color(0), Color(0), Color(0), Color(0), Color(0), Color(0),
+        Color(0), Color(0), Color(0), Color(0), Color(0), Color(0), Color(0),
+        Color(0), Color(0), Color(0), Color(0), Color(0), Color(0), Color(0),
+        Color(0), Color(0), Color(0), Color(0))
+
+    // TODO: Choose distinct colors
+    val darkColorScheme: ColorScheme = ColorScheme(Color(-1L), Color(-1L),
+        Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L),
+        Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L),
+        Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L),
+        Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L),
+        Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L),
+        Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L),
+        Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L), Color(-1L),
+        Color(-1L), Color(-1L), Color(-1L), Color(-1L))
+
 }
