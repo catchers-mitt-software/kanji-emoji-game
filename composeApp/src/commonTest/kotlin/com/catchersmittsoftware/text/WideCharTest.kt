a0c1c542e05a159c31e66f30690c30cd1fc98dc1
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-19 22:55:57 -0400

    Preparing to test WideChar

--- composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
@@ -1,9 +1,15 @@
 package com.catchersmittsoftware.text
 
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class WideCharTest {
+
+    fun pickNonBMPCodepoint(): Int {
+        return Random.nextInt(0x17000, 0x187FF)
+    }
+
 }
