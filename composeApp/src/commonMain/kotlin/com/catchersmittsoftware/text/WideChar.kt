66b85ae22e6dd4734d2934218310a331a2ce7927
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-22 00:10:37 -0400

    Passing toString( ) test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/WideChar.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/WideChar.kt
@@ -2,6 +2,10 @@ package com.catchersmittsoftware.text
 
 class WideChar(val codePoint: Int) {
 
+    override fun toString(): String {
+        return this.codePoint.toChar().toString()
+    }
+
     constructor(ch: Char) : this(ch.code)
 
 }
