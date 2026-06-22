d8b582bbfc50d654245be2f2e75f1f30a6ead6ce
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-22 14:54:37 -0400

    New file and test class

---
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/SurrogatesProcessor.kt
@@ -0,0 +1,11 @@
+package com.catchersmittsoftware.text
+
+// TODO: Write tests for this
+fun breakIntoSurrogates(codePoint: Int): Pair<Char, Char> {
+    return Pair('?', 'X')
+}
+
+// TODO: Write tests for this
+fun assembleFromSurrogates(pair: Pair<Char, Char>): WideChar {
+    return WideChar(0x10002)
+}
---
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/SurrogatesProcessorTest.kt
@@ -0,0 +1,8 @@
+package com.catchersmittsoftware.text
+
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SurrogatesProcessorTest {
+}
