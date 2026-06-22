ad89d3d17a882f51ff50098badd18be70c373d63
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-22 00:21:50 -0400

    Corrected toString( ) test for aux constructor; failing

--- composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
@@ -6,13 +6,17 @@ import kotlin.test.assertEquals
 
 class WideCharTest {
 
+    fun pickBMPKanji(): Char {
+        return Random.nextInt(0x4E00, 0xA000).toChar()
+    }
+
     fun pickNonBMPCodepoint(): Int {
         return Random.nextInt(0x17000, 0x18CD5)
     }
 
     @Test
-    fun testToString() {
-        val ch = pickNonBMPCodepoint().toChar()
+    fun testToStringAuxConstructor() {
+        val ch = pickBMPKanji()
         val instance = WideChar(ch)
         val expected = ch.toString()
         val actual = instance.toString()
