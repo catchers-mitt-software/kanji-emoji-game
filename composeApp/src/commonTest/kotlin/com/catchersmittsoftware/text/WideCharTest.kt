ce251ccac955557a5d0ac452ff99cc263184b5a4
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-21 23:59:35 -0400

    Failing toString( ) test

--- composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
@@ -3,13 +3,20 @@ package com.catchersmittsoftware.text
 import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
 class WideCharTest {
 
     fun pickNonBMPCodepoint(): Int {
         return Random.nextInt(0x17000, 0x18CD5)
+    }
+
+    @Test
+    fun testToString() {
+        val ch = pickNonBMPCodepoint().toChar()
+        val instance = WideChar(ch)
+        val expected = ch.toString()
+        val actual = instance.toString()
+        assertEquals(expected, actual)
     }
 
 }
