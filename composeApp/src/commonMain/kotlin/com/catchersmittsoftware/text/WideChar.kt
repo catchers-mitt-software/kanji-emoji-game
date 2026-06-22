40d411a90bc155d6c2daa87b1c123d1d049f7bcf
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-22 00:12:49 -0400

    Rewind toString( ) to failing

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/WideChar.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/WideChar.kt
@@ -3,7 +3,7 @@ package com.catchersmittsoftware.text
 class WideChar(val codePoint: Int) {
 
     override fun toString(): String {
-        return this.codePoint.toChar().toString()
+        return this.codePoint.toString()
     }
 
     constructor(ch: Char) : this(ch.code)
