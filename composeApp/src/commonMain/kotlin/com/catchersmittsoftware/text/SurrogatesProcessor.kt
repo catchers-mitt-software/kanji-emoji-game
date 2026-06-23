dee5394e3899fd5df9b3ecf7866989844f6d766e
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-23 15:30:50 -0400

    Changed to object

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/SurrogatesProcessor.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/SurrogatesProcessor.kt
@@ -1,11 +1,15 @@
 package com.catchersmittsoftware.text
 
-// TODO: Write tests for this
-fun assembleFromSurrogates(pair: Pair<Char, Char>): WideChar {
-    return WideChar(0x10002)
-}
+object SurrogatesProcessor {
 
-// TODO: Write tests for this
-fun breakIntoSurrogates(codePoint: Int): Pair<Char, Char> {
-    return Pair('?', 'X')
+    // TODO: Write tests for this
+    fun assembleFromSurrogates(pair: Pair<Char, Char>): WideChar {
+        return WideChar(0x10002)
+    }
+
+    // TODO: Write tests for this
+    fun breakIntoSurrogates(codePoint: Int): Pair<Char, Char> {
+        return Pair('?', 'X')
+    }
+
 }
