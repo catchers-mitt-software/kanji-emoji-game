5ce8818e578ebfefebb51061a9e82543628f4b4b
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-23 15:11:24 -0400

    Changed order of stubs

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/SurrogatesProcessor.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/SurrogatesProcessor.kt
@@ -1,11 +1,11 @@
 package com.catchersmittsoftware.text
 
 // TODO: Write tests for this
-fun breakIntoSurrogates(codePoint: Int): Pair<Char, Char> {
-    return Pair('?', 'X')
+fun assembleFromSurrogates(pair: Pair<Char, Char>): WideChar {
+    return WideChar(0x10002)
 }
 
 // TODO: Write tests for this
-fun assembleFromSurrogates(pair: Pair<Char, Char>): WideChar {
-    return WideChar(0x10002)
+fun breakIntoSurrogates(codePoint: Int): Pair<Char, Char> {
+    return Pair('?', 'X')
 }
