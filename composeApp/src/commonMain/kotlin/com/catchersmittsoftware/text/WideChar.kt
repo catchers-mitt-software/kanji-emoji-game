f18307e1d9c33671f1f023341444b88130c92cac
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-23 15:19:08 -0400

    equals( ) and hashCode( ) stubs

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/WideChar.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/WideChar.kt
@@ -1,9 +1,20 @@
 package com.catchersmittsoftware.text
 
+// TODO: Convert to data class after all tests are passing
 class WideChar(val codePoint: Int) {
 
     override fun toString(): String {
         return this.codePoint.toString()
+    }
+
+    // TODO: Write tests for this
+    override fun equals(other: Any?): Boolean {
+        return false
+    }
+
+    // TODO: Write tests for this
+    override fun hashCode(): Int {
+        return Int.MAX_VALUE
     }
 
     constructor(ch: Char) : this(ch.code)
