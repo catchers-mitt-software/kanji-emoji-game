334d36788e673fffffb346e5de54679ac2ab71c3
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-26 23:16:40 -0400

    Failing both toString( ) tests

--- composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/WideCharTest.kt
@@ -11,7 +11,14 @@ class WideCharTest {
     }
 
     private fun pickNonBMPCodepoint(): Int {
-        return Random.nextInt(0x17000, 0x18CD5)
+        return Random.nextInt(0x1F300, 0x1F600)
+    }
+
+    private fun convertToSurrogatePairString(codePoint: Int): String {
+        val high = SurrogatesProcessor.highSurrogate(codePoint)
+        val low = SurrogatesProcessor.lowSurrogate(codePoint)
+        val surrogates = charArrayOf(high, low)
+        return surrogates.concatToString()
     }
 
     @Test
@@ -19,6 +26,15 @@ class WideCharTest {
         val ch = pickBMPKanji()
         val instance = WideChar(ch)
         val expected = ch.toString()
+        val actual = instance.toString()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testToString() {
+        val codePoint = pickNonBMPCodepoint()
+        val instance = WideChar(codePoint)
+        val expected = convertToSurrogatePairString(codePoint)
         val actual = instance.toString()
         assertEquals(expected, actual)
     }
