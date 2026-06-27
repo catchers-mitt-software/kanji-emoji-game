98b2c533bb06b1fdab6897e289b00182e718d7e4
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-26 23:20:30 -0400

    Passing both toString( ) tests

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/WideChar.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/WideChar.kt
@@ -4,7 +4,14 @@ package com.catchersmittsoftware.text
 class WideChar(val codePoint: Int) {
 
     override fun toString(): String {
-        return this.codePoint.toString()
+        if (this.codePoint < 0x10000) {
+            return this.codePoint.toChar().toString()
+        } else {
+            val high = SurrogatesProcessor.highSurrogate(this.codePoint)
+            val low = SurrogatesProcessor.lowSurrogate(this.codePoint)
+            val surrogates = charArrayOf(high, low)
+            return surrogates.concatToString()
+        }
     }
 
     // TODO: Write tests for this
