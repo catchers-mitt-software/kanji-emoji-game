2fa9cd1869f7fa72a916c09093b24fb0e821f600
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-10 02:06:58 -0400

    Preparing to fail the next test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -3,7 +3,8 @@ package com.catchersmittsoftware.text
 fun isCJKIdeographic(ch: Char): Boolean {
     return if (ch in '\u3220'..'\u3247') {
         true
-    } else (ch in ('\u3280'..<'\u32CC')) || (ch > '\u4DFF')
+    } else (ch in ('\u3280'..<'\u32CC')) || (ch in ('\u32D0' .. '\u32FE')) ||
+            (ch > '\u4DFF')
 }
 
 // TODO: Write tests for this
