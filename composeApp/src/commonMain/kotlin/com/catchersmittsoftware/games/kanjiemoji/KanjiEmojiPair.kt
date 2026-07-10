8c208c5743e0b89377cfa92c3748fda56d4f1461
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-10 19:24:20 -0400

    Passing toString( ) test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/KanjiEmojiPair.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/KanjiEmojiPair.kt
@@ -2,5 +2,9 @@ package com.catchersmittsoftware.games.kanjiemoji
 
 import com.catchersmittsoftware.text.WideChar
 
-class KanjiEmojiPair(kanji: Char, emoji: WideChar) {
+// TODO: Refactor as a data class once all tests are passing
+class KanjiEmojiPair(val kanji: Char, val emoji: WideChar) {
+
+    override fun toString(): String = "($kanji, $emoji)"
+    
 }
