93efb96e13adafb23dc2a9da568e2051c425d25d
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-13 23:46:57 -0400

    Refactored as a data class

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/KanjiEmojiPair.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/KanjiEmojiPair.kt
@@ -2,17 +2,4 @@ package com.catchersmittsoftware.games.kanjiemoji
 
 import com.catchersmittsoftware.text.WideChar
 
-// TODO: Refactor as a data class once all tests are passing
-class KanjiEmojiPair(val kanji: Char, val emoji: WideChar) {
-
-    override fun toString(): String = "($kanji, $emoji)"
-
-    override fun equals(other: Any?): Boolean = if (other is KanjiEmojiPair) {
-        return this.kanji == other.kanji && this.emoji == other.emoji
-    } else {
-        return false
-    }
-
-    override fun hashCode(): Int = (kanji.hashCode() shl 16) + emoji.hashCode()
-    
-}
+data class KanjiEmojiPair(val kanji: Char, val emoji: WideChar)
