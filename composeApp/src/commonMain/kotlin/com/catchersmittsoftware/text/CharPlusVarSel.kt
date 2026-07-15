189a4fc2e74d2ddd31e986e97a17e2dc24508dfa
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-15 01:53:36 -0400

    Passing both toString( ) tests

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharPlusVarSel.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharPlusVarSel.kt
@@ -2,7 +2,11 @@ package com.catchersmittsoftware.text
 
 class CharPlusVarSel(val character: WideChar, val selector: Byte) {
 
-    // TODO: Write tests for this
-    constructor(character: Char, selector: Byte) : this(WideChar('?'), selector)
+    override fun toString(): String {
+        return character.toString() + (65024 + selector).toChar()
+    }
+
+    constructor(character: Char, selector: Byte) :
+            this(WideChar(character), selector)
 
 }
