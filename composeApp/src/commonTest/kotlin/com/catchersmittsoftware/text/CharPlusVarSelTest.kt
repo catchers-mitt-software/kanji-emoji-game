0271904d71c36747faa9957cb08a0bc3f63a805f
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-15 01:35:13 -0400

    Preparing to start testing

--- composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/CharPlusVarSelTest.kt
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/CharPlusVarSelTest.kt
@@ -1,9 +1,19 @@
 package com.catchersmittsoftware.text
 
 import kotlin.random.Random
+import kotlin.random.nextInt
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertNotEquals
 
-class CharPlusVarSelTest
+class CharPlusVarSelTest {
+
+    private fun chooseBMPMiscSymbol(): Char =
+        Random.nextInt(0x2600, 0x2700).toChar()
+
+    private fun chooseVarSel(): Byte = Random.nextInt(0x10).toByte()
+
+    // TODO: Write toString() test for aux constructor
+
+}
