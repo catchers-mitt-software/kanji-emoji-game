4a19398c47a502a67ccf2666dd7e213c76e8aa94
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-14 22:36:46 -0400

    Class-level documentation

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/KanjiEmojiPair.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/games/kanjiemoji/KanjiEmojiPair.kt
@@ -2,4 +2,13 @@ package com.catchersmittsoftware.games.kanjiemoji
 
 import com.catchersmittsoftware.text.WideChar
 
+/**
+ * Holds a pair of a kanji from the Basic Multilingual Plane (BMP) and an emoji
+ * that may be from any plane.
+ * @param kanji A character from the BMP, preferably from the CJK Unified
+ * Ideographs block, but this is not enforced. For example, '&#x5C71;'.
+ * @param emoji A character from any plane, preferably from a symbols and
+ * pictographs block in the Supplementary Multilingual Plane, but this is not
+ * enforced. For example, '&#x26F0;&#xFE0F;'.
+ */
 data class KanjiEmojiPair(val kanji: Char, val emoji: WideChar)
