e52f5923f6de98b24699fa92de2181e5a3c620b2
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-16 00:05:54 -0400

    Passing telegraph hour symbols are ideographic test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -1,9 +1,11 @@
 package com.catchersmittsoftware.text
 
+// TODO: Refactor to be more elegant
 fun isCJKIdeographic(ch: Char): Boolean {
     return if (ch in '\u3220'..'\u3247') {
         true
-    } else (ch in ('\u3280'..<'\u32CC')) || (ch == '\u32FF') || (ch > '\u4DFF')
+    } else (ch in ('\u3280'..<'\u32CC')) || (ch == '\u32FF') ||
+            (ch > '\u3357') || (ch > '\u4DFF')
 }
 
 // TODO: Write tests for this
