909ed9e416797498077d69bca70757ba8c4c28bf
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-22 16:08:37 -0400

    Passing Japanese era names are ideographic test

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/CharacterTypeIdentifier.kt
@@ -5,7 +5,8 @@ fun isCJKIdeographic(ch: Char): Boolean {
     return if (ch in '\u3220'..'\u3247') {
         true
     } else (ch in ('\u3280'..<'\u32CC')) || (ch == '\u32FF') ||
-            (ch in ('\u3358' .. '\u3370')) || (ch > '\u4DFF')
+            (ch in ('\u3358' .. '\u3370')) || (ch in ('\u337B' .. '\u337E')) ||
+            (ch > '\u4DFF')
 }
 
 // TODO: Write tests for this
